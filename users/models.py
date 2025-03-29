49fc9fc6db2b11c1012cf0d3d5b1c25322745ffb
Artikbaev Javlon <artjavlon@gmail.com>
2025-03-29 12:57:20 +0500

    Fixed password max_length bug and added views and serializers to the users app.

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ class User(AbstractBaseUser):
 
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=8)
+    password = models.CharField()
     image = models.ImageField(upload_to='users/', null=True, blank=True)
     role = models.CharField(max_length=20,
                             choices=Roles.choices,
--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
+from django.core.validators import MinLengthValidator
 from rest_framework import serializers
 
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[MinLengthValidator(8)])
+
     class Meta:
         model = User
-        fields = ['id', 'email', 'image', 'role']
+        fields = ['id', 'email', 'password', 'image', 'role']
